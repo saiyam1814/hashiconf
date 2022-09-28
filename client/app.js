dfe86040cd303fc0b91ddc93ef00e7b1076cf466
Saiyam Pathak <saiyam911@gmail.com>
2022-09-28 12:29:50 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -7,7 +7,7 @@ var btnWorshop3 = document.getElementById("work-3");
 
 // __________________________________________________________________
 
-var URL = "https://waypoint.5a3b223a-d26d-4cc8-bf7a-b6e84b0e9b09.lb.civo.com" ;
+var URL = "https://waypoint.1a9cb0d6-74fb-4000-9b95-07159d2a3989.lb.civo.com" ;
 
 btnWorshop1.addEventListener("click", function (e) {
   e.preventDefault();
